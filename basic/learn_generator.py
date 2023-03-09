19f65b9ba9c3afafc09fb55f6a1d41585d03514a
fetasty <fetasty@outlook.com>
2023-03-09 08:17:49 +0800

    docs(basic): 基本生成器注释

--- basic/learn_generator.py
+++ basic/learn_generator.py
@@ -1,6 +1,9 @@
+# 生成式定义的生成器, 语法上与列表生成式类似
 def expr_generator(a: int, b: int):
   return (x for x in range(a, b))
 
+# 函数定义的生成器, yield执行后, 返回一个值, 暂存当前的函数状态并返回(让出CPU)
+# 当下次next调用或者迭代时, 继续从上次yield之后的位置执行, 直到再次碰到yield放弃CPU
 def fib_generator(n: int):
   a = 0
   b = 1
